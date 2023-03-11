9203f68ba982d19e52298c21e54741e8231b92d9
Anguie02 <1487186@senati.pe>
2023-03-11 10:51:55 -0500

    Creamos un objeto llamado entrada1 de la clase scanner

---
+++ Ejercicios_Java/Ejercicio1_java/src/ejercicio1_java/Ejercicio1_java.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio1_java;
+
+/**
+ *
+ * @author Alumno
+ */
+//Importamos 
+import java.util.Scanner;
+
+public class Ejercicio1_java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //creamos un objeto de la clase Scanner
+        Scanner  entrada = new Scanner(System.in);
+        int numero;
+        
+        
+        
+    }
+    
+}
