022328d8744428af52d00637c0e22c50fcabd580
Anguie02 <1487186@senati.pe>
2023-03-15 00:06:41 -0500

    Pedir el ingreso de un número y mostrarlo

--- Ejercicios_1semana/Entrada_Salida_Datos/src/entrada_salida_datos/Entrada_Salida_Datos.java
+++ Ejercicios_1semana/Entrada_Salida_Datos/src/entrada_salida_datos/Entrada_Salida_Datos.java
@@ -2,19 +2,28 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package entrada_salida_datos;
 
-/**
- *
- * @author Enrique
- */
+package entrada_salida_datos;
+// Importamos la clase escaner
+import java.util.*;
+
 public class Entrada_Salida_Datos {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Creamos  un objeto de la clase escaner para 
+        // almacenar la entrada de datos
+        Scanner entrada = new Scanner(System.in);
+        //Mostramos un texto que pida el ingreso  de un número
+        System.out.println("Ingrese un  número entero: ");
+        //Declaramos una variable de tipo int para 
+        //almacenar el ingreso del número
+        int numero = entrada.nextInt();
+        //mostramos el número
+        System.out.println("El número ingresado es:"+ numero);
+        
     }
     
 }
