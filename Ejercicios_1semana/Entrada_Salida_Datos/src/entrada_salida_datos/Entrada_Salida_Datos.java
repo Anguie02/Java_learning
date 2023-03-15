f9144998d477e89594c03cc9e4e0a632871bc034
Anguie02 <1487186@senati.pe>
2023-03-14 23:33:37 -0500

    Actualizar repositorio

--- Ejercicios_1semana/Entrada_Salida_Datos/src/entrada_salida_datos/Entrada_Salida_Datos.java
+++ Ejercicios_1semana/Entrada_Salida_Datos/src/entrada_salida_datos/Entrada_Salida_Datos.java
@@ -2,28 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package ejercicio1_java;
+package entrada_salida_datos;
 
 /**
  *
- * @author Alumno
+ * @author Enrique
  */
-//Importamos 
-import java.util.Scanner;
-
-public class Ejercicio1_java {
+public class Entrada_Salida_Datos {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        //creamos un objeto de la clase Scanner
-        Scanner  entrada = new Scanner(System.in);
-        int numero;
-        
-        
-        
     }
     
 }
