407f741c9990342fe4d65ec993772969a1cdbbc9
Angui02 <1487186@senati.pe>
2023-03-16 09:25:05 -0500

    Pedimos el ingreso de un número

---
+++ Ejercicios_2semana/condicional_if_ejercicio/src/condicional_if_ejercicio/Condicional_if_ejercicio.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package condicional_if_ejercicio;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Condicional_if_ejercicio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
