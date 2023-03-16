d7024a7eb1f03838fb3654f95b3dfa308f10c729
Angui02 <1487186@senati.pe>
2023-03-16 10:19:40 -0500

    Comprobar si el número ingresado es  mayor o menor que 10 (IF)

--- Ejercicios_2semana/condicional_if_ejercicio/src/condicional_if_ejercicio/Condicional_if_ejercicio.java
+++ Ejercicios_2semana/condicional_if_ejercicio/src/condicional_if_ejercicio/Condicional_if_ejercicio.java
@@ -4,17 +4,33 @@
  */
 package condicional_if_ejercicio;
 
-/**
- *
- * @author Alumno
- */
+// imortamos Scanner
+import java.util.*;
 public class Condicional_if_ejercicio {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Creamos un objeto  de la clase escaner 
+        Scanner entrada = new Scanner(System.in);
+        //Pedimos el ingreso de un numero
+        System.out.println("Ingrese un número: ");
+        //Declaramos una variable para almacener un dato de tipo entero
+        int number = entrada.nextInt();
+        
+        //Creamos una condicional para verificar si 
+        //un numero es mayor o menor que 10
+        // Si es menor 
+        if ( number < 10){
+            System.out.println("El número "+ number + " es menor que 10");
+        // de lo contrario
+        }else{
+            System.out.println("El número " + number+ " es mayor que 10");
+        }
+        
+        
+        
+        
     }
-    
-}
+}        
