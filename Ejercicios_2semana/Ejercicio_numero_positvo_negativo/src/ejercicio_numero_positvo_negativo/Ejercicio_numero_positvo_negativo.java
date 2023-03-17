5ab6ff8516113128ea8cad4a7f7f1f09f3f44c17
Anguie02 <1487186@senati.pe>
2023-03-17 10:52:18 -0500

    Analizamos con el número 0

--- Ejercicios_2semana/Ejercicio_numero_positvo_negativo/src/ejercicio_numero_positvo_negativo/Ejercicio_numero_positvo_negativo.java
+++ Ejercicios_2semana/Ejercicio_numero_positvo_negativo/src/ejercicio_numero_positvo_negativo/Ejercicio_numero_positvo_negativo.java
@@ -11,23 +11,27 @@ public class Ejercicio_numero_positvo_negativo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         //Ejercicio que muestre si un número es negativo o positivo
         //Creamos un objeto de la clase scanner
         Scanner entrada = new Scanner(System.in);
-        //Pedimos el ingreso del nombre del usuario
+        //Pedimos el ingreso del nombre del usuario y lo guardamos en una variable
         System.out.println("Ingrese su nombre :");
         String name_user = entrada.nextLine();
         //Mostramos el nombre del usuario y le pedimos que ingrese un número
         System.out.println("Hola "+ name_user);
-        System.out.println("Ingrese un número");
+        System.out.println("Ingrese un número diferente a 0: ");
         //Guardamos el número en una variable
         int number_in = entrada.nextInt();
         //Crreamos una condicional para verificar si el número es positivo o negativo
         if (number_in < 0){
             System.out.println("El número ingresado es negativo");
+        }
+            //Agregamos una condicional si es que el usuario ingresa 0
+        else if ( number_in == 0) {
+            System.out.println("Eror, ingrese el número nuevamente");
         }else{
-            System.out.println("El número ingresado es positivo");
+            System.out.println("El númeo ingresado es positivo");
         }
         
     }
