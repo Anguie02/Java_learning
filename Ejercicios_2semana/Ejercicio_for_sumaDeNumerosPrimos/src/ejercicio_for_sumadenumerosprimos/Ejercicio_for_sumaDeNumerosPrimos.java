b959300826a15aaed0bd906ab324f3d3a2484238
Anguie02 <1487186@senati.pe>
2023-03-18 10:46:39 -0500

    Pedimos el ingreso de un número y lo guardamos en una variable

---
+++ Ejercicios_2semana/Ejercicio_for_sumaDeNumerosPrimos/src/ejercicio_for_sumadenumerosprimos/Ejercicio_for_sumaDeNumerosPrimos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio_for_sumadenumerosprimos;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Ejercicio_for_sumaDeNumerosPrimos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
