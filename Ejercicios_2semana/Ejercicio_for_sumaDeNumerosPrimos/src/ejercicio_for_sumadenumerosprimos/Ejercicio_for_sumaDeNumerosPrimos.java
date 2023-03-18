b95d0da32eebba9b7776451f389de7ce2796f8a5
Anguie02 <1487186@senati.pe>
2023-03-18 10:59:59 -0500

    Creamos un bucle for para la suma

--- Ejercicios_2semana/Ejercicio_for_sumaDeNumerosPrimos/src/ejercicio_for_sumadenumerosprimos/Ejercicio_for_sumaDeNumerosPrimos.java
+++ Ejercicios_2semana/Ejercicio_for_sumaDeNumerosPrimos/src/ejercicio_for_sumadenumerosprimos/Ejercicio_for_sumaDeNumerosPrimos.java
@@ -3,11 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package ejercicio_for_sumadenumerosprimos;
-
-/**
- *
- * @author Alumno
- */
+import java.util.*;
 public class Ejercicio_for_sumaDeNumerosPrimos {
 
     /**
@@ -15,6 +11,18 @@ public class Ejercicio_for_sumaDeNumerosPrimos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        //Pedimos el ingreso de un número y lo guardamos
+        System.out.println("Ingrese un número:");
+        int numero_ingresado = entrada.nextInt();
+        //Creamos el bucle para realizar la suma de numeros pares
+        //Inicializamos la variable suma
+        int suma = 0;
+        for (int a=2; a <= numero_ingresado; a += 2){
+           suma = suma + a ;
+        }
+        
+        System.out.println("La suma de los primeros numeros pares es: " + suma);
     }
     
 }
