62a1843f61934d562f260164237d75a3168831a4
Anguie02 <1487186@senai.pe>
2023-03-18 08:18:16 -0500

    Actualizar comentarios

--- Ejercicios_2semana/Ejercicio_blucle_for/src/ejercicio_blucle_for/Ejercicio_blucle_for.java
+++ Ejercicios_2semana/Ejercicio_blucle_for/src/ejercicio_blucle_for/Ejercicio_blucle_for.java
@@ -4,13 +4,12 @@
  */
 package ejercicio_blucle_for;
 
-import java.util.*
+import java.util.*;
 public class Ejercicio_blucle_for {
 
     
     public static void main(String[] args) {
         // Algoritmo que muestre la suma de los 10 primeros numeros naturales
-        
         Scanner entrada = new Scanner(System.in);
         //Pedimos el ingreso de un número
          System.out.println("Ingrese un número: ");
@@ -19,10 +18,14 @@ public class Ejercicio_blucle_for {
          //Creamos un bucle for para realizar la suma de los números
          //Inicializamos la variable suma
          int suma = 0;
+         //Primer parametro: inicio
+         //Segundo parametro: Límite , un boleano, el blucle se ejecuta mientras esa condición se cumpla
+         //Tercer: Incremento
          for (int numero = 1; numero <= numero_ingresado ; numero ++ ){
-            System.out.println(numero);
+            System.out.println(numero );
             suma = suma + numero;
-                    }
+                   }
+         //Mostramos la suma
          System.out.println("La suma es: "+ suma);
         
         
