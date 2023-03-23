5acdf85c5c543d35d6ee655c715530a3adda4de5
Anguie02 <1487186@senati.pe>
2023-03-23 10:02:21 -0500

    Ingresar un número y almacenarlo en una variable

---
+++ Ejercicios_3semana/While_numerosPares_Impares/src/while_numerospares_impares/While_numerosPares_Impares.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package while_numerospares_impares;
+
+/**
+ *
+ * @author Alumno
+ */
+public class While_numerosPares_Impares {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
