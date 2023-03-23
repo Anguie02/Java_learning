8564b3c32aae248b74b7744ffe7df1ece8f5beec
Anguie02 <1487186@senati.pe>
2023-03-23 10:46:12 -0500

    Crear un bucle while y una condicional para verificar si el número es par o impar

--- Ejercicios_3semana/While_numerosPares_Impares/src/while_numerospares_impares/While_numerosPares_Impares.java
+++ Ejercicios_3semana/While_numerosPares_Impares/src/while_numerospares_impares/While_numerosPares_Impares.java
@@ -1,20 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package while_numerospares_impares;
 
-/**
- *
- * @author Alumno
- */
+package while_numerospares_impares;
+//Imortamos la clase Scanner
+import java.util.*;
+
 public class While_numerosPares_Impares {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Creamos un objeto
+        Scanner entrada = new Scanner(System.in);
+        //Pedimos el ingreso de un número
+        System.out.println("Ingrese un número: ");
+        //Lo guardamos en una variable
+        int dato_numerico = entrada.nextInt();
+        //Creamos un bucle while para iterar , sumar y mostrar los números pares
+        //Inicializamos una variable para iterar
+        int i = 0;
+        //Inicializamos una variable para almacenar la suma
+        int suma = 0;
+        //Creamos 
+        while (i<= dato_numerico){
+        //Creamos una condicional para verificar si el número es par
+            if (i % 2 == 0){
+            System.out.println("Números pares: "+ i);
+            suma= suma + i ;
+            i= i + 1;
+            }else  {
+                //System.out.println("Números impares: "+ i);
+                i=i+1;
+            }
+            
+        }
+        System.out.println("La suma es : "+ suma);
+    
     }
     
 }
