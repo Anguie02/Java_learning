987ae203c04df90dd7ec42d92639134063838403
Anguie02 <1487186@senati.pe>
2023-03-23 08:34:00 -0500

    Incremento de dos

--- Ejercicios_2semana/Ejercicio_for_sumaDeNumerosPrimos/src/ejercicio_for_sumadenumerosprimos/Ejercicio_for_sumaDeNumerosPrimos.java
+++ Ejercicios_2semana/Ejercicio_for_sumaDeNumerosPrimos/src/ejercicio_for_sumadenumerosprimos/Ejercicio_for_sumaDeNumerosPrimos.java
@@ -18,7 +18,7 @@ public class Ejercicio_for_sumaDeNumerosPrimos {
         //Creamos el bucle para realizar la suma de numeros pares
         //Inicializamos la variable suma
         int suma = 0;
-        for (int a=2; a <= numero_ingresado; a += 2){
+        for (int a=2; a <= numero_ingresado; a = a +2){
            suma = suma + a ;
         }
         
