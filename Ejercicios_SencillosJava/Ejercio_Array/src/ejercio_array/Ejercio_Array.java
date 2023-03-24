c05bd0861aebd6419af2035d5326695ecf00cd7d
Anguie <1487186@senati.pe>
2023-03-24 09:30:41 -0500

    Leer un array

---
+++ Ejercicios_SencillosJava/Ejercio_Array/src/ejercio_array/Ejercio_Array.java
@@ -0,0 +1,18 @@
+
+package ejercio_array;
+
+
+public class Ejercio_Array {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] arr = new int[2];
+
+for(int i=0; i < arr.length; i++){
+
+  System.out.println("Java");
+    }
+    
+}
+}
