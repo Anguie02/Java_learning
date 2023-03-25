4b6b642bee617d7bed52895b7bdb8a69bbe39d15
Anguie02 <1487186@senati.pe>
2023-03-25 09:27:05 -0500

    Imprimir números impares

---
+++ Ejercicios_SencillosJava/Numeros_impares_con_for/src/numeros_impares_con_for/Numeros_impares_con_for.java
@@ -0,0 +1,18 @@
+
+package numeros_impares_con_for;
+
+
+public class Numeros_impares_con_for {
+
+    
+    public static void main(String[] args) {
+        //Imprimir numeros
+        //Inicializamos una variable
+        int numero_impar = 999;
+        for(int i = 1; i<= numero_impar; i += 2){
+            System.out.println("Númro impar: "+ i);
+        
+        }
+    }
+    
+}
